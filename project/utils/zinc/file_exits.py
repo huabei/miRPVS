316dbe1f2a9521dee1af8bfe5b912c5238e13a65
王华北 <951946366@qq.com>
2022-09-29 16:02:32 +0800

    add python dock scripts

--- project/utils/zinc/file_exits.py
+++ project/utils/zinc/file_exits.py
@@ -6,8 +6,10 @@
 import os
 import gzip
 
-from sympy import re, root
+import re
 import pickle
+from tqdm import tqdm
+import random
 
 
 def check_exists(index_file, root_dir):
@@ -23,17 +25,23 @@ def check_exists(index_file, root_dir):
                 not_exists_file.append(f_path)
     return not_exists_file
 
+
 def zinc_id_find(f_path):
     # zf = gzip.open(f_path, mode='rb')
     # content = zf.read().decode()
     return re.findall('Name = (.*?)\n', gzip.open(f_path, mode='rb').read().decode())
 
 
+def pdbqt_split(f_path) -> list:
+    """generate a list of pdbqt from a gz file"""
+    return re.findall('(MODEL.*?ENDMDL\n)', gzip.open(f_path, mode='rb').read().decode(), re.S)
+
+
 def get_zinc_id_index(index_file, root_dir):
     # this function to get zinc_id from every pdbqt file
     zinc_index = dict()
     with open(index_file, 'r') as f:
-        for f_path in f:
+        for f_path in tqdm(f):
             f_path = os.path.join(root_dir, f_path.strip())
             zinc_id = zinc_id_find(f_path)
             f_name = f_path.split('/')[-1]
@@ -41,13 +49,31 @@ def get_zinc_id_index(index_file, root_dir):
     return zinc_index
 
 
+def random_accept(ratio):
+    "generate a random True with ratio "
+    if random.random() < ratio:
+        return True
+    else:
+        return False
+
+
+class ZincPdbqt():
+    def __init__(self, pdbqt_file):
+        self.f_str = gzip.open(pdbqt_file, mode='rb').read().decode()
+        self.zinc_id = re.findall('Name = (.*?)\n', self.f_str)
+        self.molecules = re.findall('MODEL.*?\n(.*?)ENDMDL\n', self.f_str, re.S)
+        self.model = zip(self.zinc_id, self.molecules)
+
+    def __iter__(self):
+        return iter(self.model)
+
+    def __len__(self):
+        return len(self.zinc_id)
+
 
 if __name__ == "__main__":
-    index_file = 'ZINC-downloader-3D-pdbqt.gz.database_index'
-    # f_path = next(f)
-    root_dir = os.getcwd()
-    zinc_pdbqt_index = get_zinc_id_index(index_file, root_dir)
-    pkl_name = 'zinc_pdbqt_3d_index.pkl'
-    with open(pkl_name, 'wb') as handle:
-        pickle.dump(zinc_pdbqt_index, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    print('--ok---'*5)
+    zinc_pdbqt_file = 'zinc_drug_like_3d_10k.pdbqt.gz'
+    ligand_pdbqt = ZincPdbqt(zinc_pdbqt_file)
+    for i in ligand_pdbqt:
+        print(i)
+        break
