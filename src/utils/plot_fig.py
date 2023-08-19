73c3a14704d3c9023a09090a14e868061ad628a0
Huabei <951946366@qq.com>
2023-08-19 14:59:29 +0800

    complete train

--- src/utils/plot_fig.py
+++ src/utils/plot_fig.py
@@ -9,7 +9,7 @@ def plot_fig(preds, targets):
     ax.scatter(targets, preds, s=1)
     ax.set_xlabel("experimental")
     ax.set_ylabel("predicted")
-    ax.set_xlim(-10, -4)
-    ax.set_ylim(-10, -4)
-    ax.plot([-10, -4], [-10, -4], color="black", linestyle="--")
+    # ax.set_xlim(-10, -4)
+    # ax.set_ylim(-10, -4)
+    ax.plot([-15, 0], [-15, 0], color="black", linestyle="--")
     return fig
