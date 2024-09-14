414ff28bc745876162b5b8c797817f3d8ffac42e
huabei <951946366@qq.com>
2024-09-14 14:07:13 +0800

    data process py scripts

--- data/create_total_dataset_hdf5.py
+++ data/create_total_dataset_hdf5.py
@@ -23,20 +23,21 @@ def analyze_zinc_pdbqt_gz(pdbqt_gz_path: str) -> Union[pd.DataFrame, pd.DataFram
     index = []
     # 读取pdbqt.gz文件
     with gzip.open(pdbqt_gz_path, 'rb') as f:
-        t_start = 0 # 记录当前分子的原子起始位置
-        t_end = 0 # 记录当前分子的原子终止位置
+        t_start = 0  # 记录当前分子的原子起始位置
+        t_end = 0  # 记录当前分子的原子终止位置
         for line in f:
             if line.startswith(b'ATOM'):
-                coor.append([str(line[12:14].strip(), 'utf-8'), float(line[30:38]), float(line[38:46]), float(line[46:54])])
-                t_end += 1 # 记录已存入原子的个数
-            if line.startswith(b'REMARK  Name = '): # 一个分子的起始位置
+                coor.append([str(line[12:14].strip(), 'utf-8'),
+                            float(line[30:38]), float(line[38:46]), float(line[46:54])])
+                t_end += 1  # 记录已存入原子的个数
+            if line.startswith(b'REMARK  Name = '):  # 一个分子的起始位置
                 if t_end == 0:
                     # 记录第一个分子的id
                     zinc_id = str(line[15:].strip(), 'utf-8')
                     continue
                 index.append([zinc_id, t_start, t_end])  # 存储上一个分子的信息
                 zinc_id = str(line[15:].strip(), 'utf-8')  # 记录当前分子的id
-                t_start = t_end # 记录当前分子的原子起始位置
+                t_start = t_end  # 记录当前分子的原子起始位置
         index.append([zinc_id, t_start, t_end])
     return pd.DataFrame(coor, columns=['atom', 'x', 'y', 'z']), pd.DataFrame(index, columns=['zinc_id', 'start', 'end']).set_index('zinc_id', drop=True)
 
@@ -56,11 +57,23 @@ def create_total_dataset_hdf5(data_folder: str) -> None:
                 for file in filename:
                     if file.endswith('.pdbqt.gz'):
                         # 构造hdf5的key，去掉ZINC_DATA_PATH的前缀，去掉后缀
-                        d = os.path.join(path.replace(data_folder, os.path.basename(data_folder)), '_'.join(file.split('.')[:2])).replace('-','_')
+                        d = os.path.join(path.replace(data_folder, os.path.basename(
+                            data_folder)), '_'.join(file.split('.')[:2])).replace('-', '_')
                         # 分析pdbqt.gz文件,加入try except是因为有些文件可能有问题
                         try:
-                            logging.info(f'processing {os.path.join(path, file)}')
-                            store_coor[d], store_index[d] = analyze_zinc_pdbqt_gz(os.path.join(path, file))
+                            logging.info(
+                                f'processing {os.path.join(path, file)}')
+                            store_coor[d], store_index[d] = analyze_zinc_pdbqt_gz(
+                                os.path.join(path, file))
                         except:
-                            logging.warning(f'error in {os.path.join(path, file)}')
+                            logging.warning(
+                                f'error in {os.path.join(path, file)}')
                             continue
+
+
+if __name__ == '__main__':
+    import sys
+    data_folder = sys.argv[1]
+    # print(data_folder)
+    create_total_dataset_hdf5(data_folder)
+    print(f'{data_folder} Construct Done! Good Job!')
